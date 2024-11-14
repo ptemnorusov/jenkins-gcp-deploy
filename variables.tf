d61452acc6d0fdfb6dc25ede2c31b396182345a5
Peter <petr.temnorusov@hotmail.com>
2024-11-14 12:58:02 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+  default = "prefab-faculty-350219"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "us-central1"
+}
